3b86782c5a616f358238ec76945bc5c7a183d8d7
amirunusov <135022478+amirunusov@users.noreply.github.com>
2024-06-05 10:07:25 +0500

    view

--- my_app1/models.py
+++ my_app1/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -10,7 +11,19 @@ class Contact(models.Model):
         return f"{self.first_name} | {self.last_name}"
 
 
+class PopularProduct(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    img = models.ImageField(null=True, blank=True)
 
-# Create your models here.
+    def __str__(self):
+        return f"{self.name} - {self.price}"
 
+
+class Newsletter(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def save(self):
+        return self.name
 
--- my_app1/views.py
+++ my_app1/views.py
@@ -1,32 +1,15 @@
 from django.shortcuts import render, redirect
-from .forms import Contact, ContactForm
+
+from my_app1.models import PopularProduct, Newsletter
+from .forms import Contact, ContactForm, NewsletterForm
 
 
 def index(request):
-    return render(request, 'index.html')
-
-
-def about(request):
-    return render(request, 'about.html')
-
-
-def shop(request):
-    return render(request, 'shop.html')
-
-def services(request):
-    return render(request, 'services.html')
-
-def blog(request):
-    return render(request, 'blog.html')
-
-def contact(request):
-    return render(request, 'contact.html')
-
-def cart(request):
-    return render(request, 'cart.html')
-
-
-
+    context = {
+        'title': ' index',
+        'products': PopularProduct.objects.all(),
+    }
+    return render(request, 'my_app1/index.html', context)
 
 
 def contact_view(request):
@@ -39,3 +22,26 @@ def contact_view(request):
         form = ContactForm()
     return render(request, 'contact.html', {'form': form})
 
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def shop(request):
+    return render(request, 'shop.html')
+
+
+def services(request):
+    return render(request, 'services.html')
+
+
+def blog(request):
+    return render(request, 'blog.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def cart(request):
+    return render(request, 'cart.html')
